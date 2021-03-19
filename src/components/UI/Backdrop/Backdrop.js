03488fec8cd17265cbded76c65da03254d9273c1
Your Name <you@example.com>
2021-03-19 02:04:56 -0700

    asdfghj

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,13 +1,9 @@
+import classes from "./Backdrop.module.css";
 
-import classes from "./Backdrop.module.classes";
-
-const Backdrop= () => {
-    return (
-        <div className={classes.Backdrop}>
-    
-
-        </div>
-    )
+const Backdrop = () => {
+  return (
+    <div className={classes.Backdrop}></div>
+  );
 }
 
 export default Backdrop;
