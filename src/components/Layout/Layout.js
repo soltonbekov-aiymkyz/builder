c5d905ff1339ca652731497cec660e6a27733073
Your Name <you@example.com>
2021-03-19 02:42:55 -0700

    connected files

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,20 @@
+
+import { useState } from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../Toolbar/Toolbar";
 import Drawer from "../Drawer/Drawer";
 
-const Layout =({ children }) => {
+const Layout = ({ children }) => {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+  
     return (
-        <div className={classes.Layout}>
-            <Toolbar />
-            <Drawer />
-      {children}
-        </div>
-    )
-}
+      <div className={classes.Layout}>
+        <Toolbar openDrawer={() => setDrawerOpen(true)} />
+        <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
+        {children}
+      </div>
+    );
+  }
+   
+
 export default Layout;
