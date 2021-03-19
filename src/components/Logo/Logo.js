6538c3402182de46f4c49db5d03480923fb608f7
Your Name <you@example.com>
2021-03-19 01:26:16 -0700

    sdfgh

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,10 @@
 import classes from "./Logo.module.css";
-
+import logo from "../../images/logosvg";
 const Logo = () => {
     return (
         <div className={classes.logo}>
     Logo
-
+   <img src={logo} alt="dkfgh" />
         </div>
     )
 }
