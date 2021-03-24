1bc160bda150112489c581354dc0e38abc2797ea
Your Name <you@example.com>
2021-03-24 01:14:37 -0700

    create new div

---
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
@@ -0,0 +1,10 @@
+import classes from "./SkyscaperControls.module.css";
+const  SkyscaperControls = () => {
+    return (
+        <div className={classes.SkyscaperControls}>
+  
+        </div>
+    )
+}
+
+export default SkyscaperControls;
---
+++ src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
@@ -0,0 +1,10 @@
+import classes from "./SkyscaperPreview.module.css";
+const  SkyscaperPreview = () => {
+    return (
+        <div className={classes.SkyscaperPreview}>
+  
+        </div>
+    )
+}
+
+export default SkyscaperPreview;
