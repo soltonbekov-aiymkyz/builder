ef5726f1f5994c5a2926eac5651051dc3aaa92d3
Your Name <you@example.com>
2021-03-24 02:17:50 -0700

    sdfg

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -1,5 +1,12 @@
 //import classes from "./SkyscaperLevel.module.css";
 const  SkyscaperLevel = ({type}) => {
+const types ={
+    restaurantfloor: {backgroundImage: `url(${restaurantfloorImage})`},
+    livingfloor: 2,
+    shopfloor: 1,
+}
+
+
     return (
         <div>{type}</div>
     )
