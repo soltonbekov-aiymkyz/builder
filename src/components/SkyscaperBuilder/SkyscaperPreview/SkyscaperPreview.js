0d893cad62f293f73e32d866a8b3e66396e983bc
Your Name <you@example.com>
2021-03-25 07:35:13 -0700

    added background color

--- src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
+++ src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
@@ -1,19 +1,22 @@
 import classes from "./SkyscaperPreview.module.css";
-import classes from "./SkyscaperLevel/SkyscaperLevel.js";
 import SkyscaperLevel from "../SkyscaperLevel/SkyscaperLevel";
 const  SkyscaperPreview = ({levels}) => {
     const result = [];
 
 for (const level in levels){
     for (let i=0; i<levels[level]; i++){
-    result.push(<SkyscaperLevel type={level}/>)
+    result.push(<SkyscaperLevel key={level + i}  type={level}/>)
 }
 }
     return (
         <div className={classes.SkyscaperPreview}>
-         {result}
-        </div>
+      <div
+        className={classes.levels}
+        style={{ backgroundColor: yelow }  }>
+        {result}
+      </div>
+    </div>
     )
 }
-
 export default SkyscaperPreview;
+
