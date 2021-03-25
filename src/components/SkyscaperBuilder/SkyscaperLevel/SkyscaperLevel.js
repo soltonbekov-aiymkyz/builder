80461630122c7835c585f74bcc3a3c333fe3cea9
Your Name <you@example.com>
2021-03-25 06:53:27 -0700

    width 35px

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -3,9 +3,9 @@ import reactDom from "react-dom";
 //import classes from "./SkyscaperLevel.module.css";
 const  SkyscaperLevel = ({type}) => {
 const types ={
-    restaurantfloor: {backgroundColor:red,width:"35px", },
-    livingfloor:  {backgroundColor:blue,width:"35px", },
-    shopfloor: 1,
+    restaurantfloor: {backgroundColor:red,width:"35px",height:"35px" },
+    livingfloor:  {backgroundColor:blue,width:"35px",height:"35px" },
+    shopfloor:   {backgroundColor:violet,width:"35px", height:"35px"},
 }
 
 
