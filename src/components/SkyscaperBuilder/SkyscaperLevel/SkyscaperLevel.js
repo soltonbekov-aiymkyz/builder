d91eb4a2a94ec6c39f279b1c8c22f4d607e36e4d
Your Name <you@example.com>
2021-03-25 07:00:16 -0700

    width with files

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -8,7 +8,25 @@ const types ={
     shopfloor:   {backgroundColor:violet,width:"35px", height:"35px"},
 }
 
-
+function getPosition(levelWidth){
+        const pizzaDiameter = 380;
+        const pizzaRadius = pizzaDiameter / 2;
+        const ingredientRadius = parseInt(ingredientWidth) / 2;
+    
+        const ingredientTop = Math.round(Math.random() * pizzaDiameter);
+        const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
+    
+        const distance = Math.sqrt(
+          Math.pow(ingredientTop - pizzaRadius, 2) + Math.pow(ingredientLeft - pizzaRadius, 2)
+        ) + ingredientRadius;
+    
+        return distance < pizzaRadius
+          ? {
+            top: ingredientTop - ingredientRadius,
+            left: ingredientLeft - ingredientRadius
+          }
+          : getPosition(ingredientWidth);
+      }
 
 
 
