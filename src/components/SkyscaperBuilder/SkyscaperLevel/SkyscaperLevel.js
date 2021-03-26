9e76e26597aee5effaad94b9c844964cb56f1d21
Your Name <you@example.com>
2021-03-26 01:56:54 -0700

    added

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -10,49 +10,25 @@ const SkyscaperLevel = ({ type }) => {
       const skyscaperDiameter = 380;
       const skyscaperRadius = skyscaperDiameter / 2;
       const levelRadius = parseInt(levelWidth) / 2;
-      const Top = Math.round(Math.random() * skyscaperDiameter);
-      const Left = Math.round(Math.random() * skyscaperDiameter);
-      const distance =
-        Math.sqrt(
-          Math.pow(Top - skyscaperRadius, 2) +
-            Math.pow(Left - skyscaperRadius, 2)
+      const levelTop = Math.round(Math.random() * skyscaperDiameter);
+      const levelLeft = Math.round(Math.random() * skyscaperDiameter);
+      const distance = Math.sqrt(
+          Math.pow(levelTop - skyscaperRadius, 2) +
+            Math.pow(levelLeft - skyscaperRadius, 2)
         ) + levelRadius;
       return distance < skyscaperRadius
         ? {
-            top: Top - levelRadius,
-            left: Left - levelRadius,
+            top: levelTop - levelRadius,
+            left: levelLeft - levelRadius,
           }
         : getPosition(levelWidth);
     }
-
-
-    //   const position = getPosition(types[type].width);
-    //   types[type].top = position.top + "px";
-    //   types[type].left = position.left + "px";
-    //   types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
-
-
-
+      const position = getPosition(types[type].width);
+      types[type].top = position.top + "px";
+      types[type].left = position.left + "px";
+      types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
     return <div className={classes.SkyscaperLevel} style={types[type]}></div>;
   };
  export default SkyscaperLevel;
-
-//  import classes from "./BunsIngredient.module.css";
-
-//  const BunsIngredient = ({ type }) => {
-//    const types = {
-//      Bun1: { backgroundColor: `red`, width: "35px", height: "35px", display: "inline-block" },
-//      Bun2: { backgroundColor: `blue`, width: "35px", height: "35px", display: "inline-block" },
-//      Bun3: { backgroundColor: `green`, width: "35px", height: "35px", display: "inline-block" },
-//      Bun4: { backgroundColor: `pink`, width: "35px", height: "35px", display: "inline-block" },
-//      Bun5: { backgroundColor: `orange`, width: "35px", height: "35px", display: "inline-block" },
-//      Bun6: { backgroundColor: `yellow`, width: "35px", height: "35px", display: "inline-block" },
-//    };
  
   
- 
- 
-//    return <div className={classes.BunsIngredient} style={types[type]}></div>;
-//  };
- 
-//  export default BunsIngredient;
