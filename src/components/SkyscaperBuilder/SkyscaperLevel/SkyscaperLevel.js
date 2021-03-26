6717c6b918ad347fb0f42443ea3845b35305a1ff
Your Name <you@example.com>
2021-03-26 00:30:44 -0700

    added files

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -31,3 +31,40 @@ const SkyscaperLevel = ({ type }) => {
     return <div className={classes.SkyscaperLevel} style={types[type]}></div>;
   };
  export default SkyscaperLevel;
+
+
+ 
+//  const SkyscaperLevel = ({ type }) => {
+//     const types = {
+//       salami: { backgroundImage: `url(${salamiBackground})`, width: "35px", height: "35px" },
+//       tomato: { backgroundImage: `url(${tomatoBackground})`, width: "35px", height: "35px" },
+//       blackOlive: { backgroundImage: `url(${blackOliveBackground})`, width: "10px", height: "10px" },
+//       greenOlive: { backgroundImage: `url(${greenOliveBackground})`, width: "10px", height: "10px" },
+//       redPepper: { backgroundImage: `url(${redPepperBackground})`, width: "20px", height: "20px" },
+//       yellowPepper: { backgroundImage: `url(${yellowPepperBackground})`, width: "40px", height: "40px" },
+//     };
+//     function getPosition(ingredientWidth) {
+//       const pizzaDiameter = 380;
+//       const pizzaRadius = pizzaDiameter / 2;
+//       const ingredientRadius = parseInt(ingredientWidth) / 2;
+//       const ingredientTop = Math.round(Math.random() * pizzaDiameter);
+//       const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
+//       const distance = Math.sqrt(
+//         Math.pow(ingredientTop - pizzaRadius, 2) + Math.pow(ingredientLeft - pizzaRadius, 2)
+//       ) + ingredientRadius;
+//       return distance < pizzaRadius
+//         ? {
+//           top: ingredientTop - ingredientRadius,
+//           left: ingredientLeft - ingredientRadius
+//         }
+//         : getPosition(ingredientWidth);
+//     }
+//     const position = getPosition(types[type].width);
+//     types[type].top = position.top + "px";
+//     types[type].left = position.left + "px";
+//     types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
+//     return (
+//       <div className={classes.AquariumFish} style={types[type]}></div>
+//     );
+//   }
+  
