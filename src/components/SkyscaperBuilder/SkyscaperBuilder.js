145e36dec48a271669b14209976d678aaff6d690
Your Name <you@example.com>
2021-03-26 01:45:14 -0700

    added file

--- src/components/SkyscaperBuilder/SkyscaperBuilder.js
+++ src/components/SkyscaperBuilder/SkyscaperBuilder.js
@@ -3,7 +3,7 @@ import SkyscaperPreview from "./SkyscaperPreview/SkyscaperPreview";
 import SkyscaperControls from "./SkyscaperControls/SkyscaperControls";
 import  { useState } from "react";
 const  SkyscaperBuilder = () => {
-    const [level, setLevels]= useStale({
+    const [levels, setLevels]= useStale({
         restaurantfloor: 10,
         livingfloor: 20,
         shopfloor: 10,
@@ -16,26 +16,3 @@ const  SkyscaperBuilder = () => {
     )
 }
 export default SkyscaperBuilder;
-
-// import classes from "./BunsBuilder.module.css";
-// import BunsControls from "./BunsControls/BunsControls";
-// import BunsPreview from "./BunsPreview/BunsPreview";
-
-// const BunsBuilder = () => {
-//     const ingredients = {
-//         Bun1: 100,
-//         Bun2: 10,
-//         Bun3: 199,
-//         Bun4: 199,
-//         Bun5: 199,
-//         Bun6: 199,
-//     };
-//     return ( 
-//         <div className = {classes.BunsBuilder}>
-//             <BunsPreview ingredients = {ingredients}/>
-//             <BunsControls />
-//         </div>
-//     );
-// }
- 
-// export default BunsBuilder;
