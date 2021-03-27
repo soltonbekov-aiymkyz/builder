361f81a183e9a23183841cf7c684ff9b95b732df
Your Name <you@example.com>
2021-03-27 03:23:34 -0700

    update file

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
@@ -1,22 +1,42 @@
 
-import classes from "./SkyscaperControl/SkyscaperControl";
-import classes from "./SkyscaperControls.module.css";
-const SkyscaperControls = ({levels,addLevel,removeLevel}) => {
-  const result =[];
-  for(const level in levels){
-    result.push(
-    <SkyscaperControl 
-    type={level} 
-    addLevel={addLevel}
-    removeLevel={removeLevel}
-    />
-    )
-  }
-  return (
-    <div className={classes.SkyscaperControls}>{results}</div>
-  );
-  };
-export default SkyscaperControls;
+// import classes from "./SkyscaperControl/SkyscaperControl";
+// import classes from "./SkyscaperControls.module.css";
+// const SkyscaperControls = ({levels,addLevel,removeLevel}) => {
+//   const result =[];
+//   for(const level in levels){
+//     result.push(
+//     <SkyscaperControl 
+//     type={level} 
+//     addLevel={addLevel}
+//     removeLevel={removeLevel}
+//     />
+//     )
+//   }
+//   return (
+//     <div className={classes.SkyscaperControls}>{results}</div>
+//   );
+//   };
+// export default SkyscaperControls;
 
+
+
+
+
+import classes from "./SkyscaperControls.module.css";
+
+const SkyscaperControls = ({levels}) => {
+  const results = [];
+  for (const level in leves) {
+   results.push(computeHeadingLevel);
+    }
+ 
+    return (
+      <div className={classes.SkyscaperControls}>
+       {results}
+      </div>
+    );
+  }
+  
+  export default SkyscaperControls;
 
 
