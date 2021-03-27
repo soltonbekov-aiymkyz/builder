c2771ca3e6b01b235c4a9edc7f1fc3e10f66d1e3
Your Name <you@example.com>
2021-03-26 23:37:21 -0700

    added file

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
@@ -1,14 +1,37 @@
 
 
+
+
+import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
 import classes from "./SkyscaperControl.module.css";
 const SkyscaperControl = ({ type,addLevel,removeLevel }) => {
     return ( 
         <div className = {classes.SkyscaperControl}>
-            <button onClick={()=>addLevel(type)}>-</button>
+            <button  className = {classes.ControlButton}  onClick={()=>addLevel(type)}>+</button>
             <SkyscaperLevel type={type} />
-            <button   onClick={()=>removeLevel(type)}  >+</button>
+            <button className = {classes.ControlButton}  onClick={()=>removeLevel(type)}  >-</button>
         </div>
     );
 }
  
 export default SkyscaperControl;
+
+
+
+
+
+
+
+
+
+const BunsControl = ({ type, addIngredient, removeIngredient }) => {
+    return ( 
+        <div className = {classes.BunsControl}>
+            <button className = {classes.ControlButton} onClick = {() => removeIngredient(type)}>-</button>
+            <BunsIngredient type = {type} />
+            <button className = {classes.ControlButton} onClick = {() => addIngredient(type)}>+</button>
+        </div>
+    );
+}
+ 
+export default BunsControl;
