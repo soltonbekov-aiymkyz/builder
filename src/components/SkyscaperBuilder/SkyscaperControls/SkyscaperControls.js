3f65f743947379f33c918d215456786a89c0117f
Your Name <you@example.com>
2021-03-26 23:18:55 -0700

    update SkyscaperControls.js

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
@@ -1,23 +1,22 @@
 
 import classes from "./SkyscaperControl/SkyscaperControl";
 import classes from "./SkyscaperControls.module.css";
-const SkyscaperControls = ({levels}) => {
+const SkyscaperControls = ({levels,addLevel,removeLevel}) => {
   const result =[];
   for(const level in levels){
-    result.push(<SkyscaperControl type={level} />)
+    result.push(
+    <SkyscaperControl 
+    type={level} 
+    addLevel={addLevel}
+    removeLevel={removeLevel}
+    />
+    )
   }
   return (
     <div className={classes.SkyscaperControls}>{results}</div>
   );
-}
+  };
 export default SkyscaperControls;
 
-// import classes from "./BunsControls.module.css";
 
-// const BunsControls = () => {
-//     return ( 
-//         <div className = {classes.BunsControls}>Controls</div>
-//     );
-// }
- 
-// export default BunsControls;
+
