7098f612a7305d1e795f3ebbff6960b774bc6a80
Your Name <you@example.com>
2021-04-01 21:13:36 -0700

    new styles for SkyscsperLevwel.js

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -2,12 +2,12 @@
 import classes from "./SkyscaperLevel.module.css";
 const SkyscaperLevel = ({ type }) => {
     const types = {
-      floor1: { backgroundColor: `red`, width: "35px", height: "35px", display: "inline-block"},
-      floor2: { backgroundColor: `blue`, width: "35px", height: "35px",display: "inline-block" },
-      floor3: { backgroundColor: `green`, width: "35px", height: "35px",display: "inline-block" },
-      floor4: { backgroundColor: `yellow`, width: "35px", height: "35px",display: "inline-block"},
-      floor5: { backgroundColor: `pink`, width: "35px", height: "35px", display: "inline-block"},
-      floor6: { backgroundColor: `orange`, width: "35px", height: "35px",display: "inline-block" },
+      floor1: { backgroundColor: `red`, width: "50px", height: "50px", display: "inline-block"},
+      floor2: { backgroundColor: `blue`, width: "50px", height: "50px",display: "inline-block" },
+      floor3: { backgroundColor: `green`, width: "50px", height: "50px",display: "inline-block" },
+      floor4: { backgroundColor: `yellow`, width: "50px", height: "50px",display: "inline-block"},
+      floor5: { backgroundColor: `pink`, width: "50px", height: "50px", display: "inline-block"},
+      floor6: { backgroundColor: `orange`, width: "50px", height: "50px",display: "inline-block" },
     }
     return <div className={classes.SkyscaperLevel} style={types[type]}></div>;
   };
