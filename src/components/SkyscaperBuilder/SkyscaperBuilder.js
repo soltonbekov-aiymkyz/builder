4714d8727e9873af020aa1a4d98f784a5e91047b
Your Name <you@example.com>
2021-04-05 07:17:29 -0700

    join two files

--- src/components/SkyscaperBuilder/SkyscaperBuilder.js
+++ src/components/SkyscaperBuilder/SkyscaperBuilder.js
@@ -2,20 +2,29 @@ import React from "react";
 import classes from "./SkyscaperBuilder.module.css";
 import SkyscaperPreview from "./SkyscaperPreview/SkyscaperPreview";
 import SkyscaperControls from "./SkyscaperControls/SkyscaperControls";
-import {useState} from "react"
-
+import axios from "axios"
+import { useEffect, useState} from "react"
 const  SkyscaperBuilder = () => {
+    const prices ={ 
+   floor1: 120000,
+   floor2:50000,
+   floor3:300,
+   floor4:41234,
+   floor5:5234,
+    }
     const [levels, setLevels]= useState({
-        floor1: 0,
-        floor2: 0,
-        floor3: 0,
-        floor4: 0,
-        floor5: 0,
-        floor5: 0,
     });
+    const [price, setPrice] = useState(150);
+        useEffect (function () {
+            axios.get('https://builder-6b86c-default-rtdb.firebaseio.com/level.json')
+                .then(response=>{
+                setLevels({...response.data});
+              });
+          }, []);
     function addLevel(type){
         const newlevels={...levels};
         newlevels[type]++;
+        setPrice(price + prices[type]);
         setLevels(newlevels);
     }
     function removeLevel(type){
@@ -23,19 +32,21 @@ const  SkyscaperBuilder = () => {
             const newLevels = { ...levels };
           newLevels[type]--;
           setLevels(newLevels);
+          setPrice(price - prices[type])
           }
     }
-
     return (
         <div className={classes.SkyscaperBuilder}>
-            <SkyscaperPreview levels = {levels}/>
+            {/* <SkyscaperPreview levels = {levels}/> */}
+            <SkyscaperPreview price={price} levels={levels} />
             <SkyscaperControls 
              levels = {levels}    
             addLevel={addLevel}
             removeLevel={removeLevel}
             />
+
         </div>
     )
 };
-export default React.memo(SkyscaperBuilder);
+export default SkyscaperBuilder;
 
--- src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
+++ src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
@@ -1,23 +1,21 @@
 import classes from "./SkyscaperPreview.module.css";
 import SkyscaperLevel from "../SkyscaperLevel/SkyscaperLevel";
-const  SkyscaperPreview = ({levels}) => {
+const  SkyscaperPreview = ({levels,price}) => {
     const result = [];
 for (const level in levels){
     for (let i=0; i<levels[level]; i++){
-    result.push(<SkyscaperLevel type={level}/>)
+    result.push(<SkyscaperLevel /* key={ingredient + i}*/ type={level}/>)
 }
 }
 return ( 
     <div className = {classes.SkyscaperPreview}>
         <div className={classes.levels}>
+            <div
+            className ={classes.levels}/>
             {result}
         </div>
+    <div className={classes.price}>{price}som</div>
     </div>
 );}
 export default SkyscaperPreview;
  
-
-
-
-
-
