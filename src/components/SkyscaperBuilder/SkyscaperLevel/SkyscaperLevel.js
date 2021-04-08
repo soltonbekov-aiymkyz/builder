56623c2641f1ba501347e30135c30a85cb6bd6a1
Your Name <you@example.com>
2021-04-08 04:27:41 -0700

    delete some misstakes in SControl

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -1,21 +1,4 @@
 
-// import classes from "./SkyscaperLevel.module.css";
-// //import flowershopp01 from "../../../images/flowershopp01.png";
-// const SkyscaperLevel = ({ type }) => {
-//     const types = {
-//     //  floor1: { backgroundColor: `url(${flowershopp01})`, width: "50px", height: "50px", },
-    
-//      floor1: { backgroundColor: `red`, width: "50px", height: "50px",display: "inline-block" },
-//       floor2: { backgroundColor: `blue`, width: "50px", height: "50px",display: "inline-block" },
-//       floor3: { backgroundColor: `green`, width: "50px", height: "50px",display: "inline-block" },
-//       floor4: { backgroundColor: `yellow`, width: "50px", height: "50px",display: "inline-block"},
-//       floor5: { backgroundColor: `pink`, width: "50px", height: "50px", display: "inline-block"},
-//       floor6: { backgroundColor: `orange`, width: "50px", height: "50px",display: "inline-block" },
-//     }
-//     return <div className={classes.SkyscaperLevel} style={types[type]}></div>;
-//   };
-//  export default SkyscaperLevel;
-
 import floor1 from "../../../Images/livingFloor.jpg"
 import floor2 from "../../../Images/bookstoree1.png"
 import floor3 from "../../../Images/changingroom.png"
