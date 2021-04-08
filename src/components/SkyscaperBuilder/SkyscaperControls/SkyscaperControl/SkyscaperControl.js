84579ba1ff416d3010d8367a080f9197e695d49b
Your Name <you@example.com>
2021-04-07 23:03:56 -0700

    changes some styles

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
@@ -1,4 +1,23 @@
 
+// import Button from "../../../UI/Button/Button";
+// import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
+// import classes from "./SkyscaperControl.module.css";
+
+// const SkyscaperControl = ({ type,addLevel,removeLevel,count }) => {
+//     return ( 
+//        <div className = {classes.SkyscaperControl}>
+//             <button   className = {classes.ControlButton} onClick={()=>addLevel(type)}  disabled={!count}>+</button>
+//            {/* <div className={classes.level}> */}
+//             <SkyscaperLevel type={type}  /*fixed*/ />
+//             {/* </div> */}
+//             <button  className = {classes.ControlButton}  onClick={()=>removeLevel(type)}  /* disabled={!count} */ >-</button>
+//         </div>
+//     );
+// }
+// export default SkyscaperControl;
+
+
+
 import Button from "../../../UI/Button/Button";
 import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
 import classes from "./SkyscaperControl.module.css";
@@ -14,5 +33,4 @@ const SkyscaperControl = ({ type,addLevel,removeLevel,count }) => {
         </div>
     );
 }
-export default SkyscaperControl;
-
+ export default SkyscaperControl;
