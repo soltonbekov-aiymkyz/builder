4923fa49f3e368f54bff5919e33ff127087f07ae
Your Name <you@example.com>
2021-04-08 02:33:53 -0700

    git push

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
@@ -18,19 +18,37 @@
 
 
 
-import Button from "../../../UI/Button/Button";
-import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
-import classes from "./SkyscaperControl.module.css";
+// import Button from "../../../UI/Button/Button";
+// import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
+// import classes from "./SkyscaperControl.module.css";
 
-const SkyscaperControl = ({ type,addLevel,removeLevel,count }) => {
+// const SkyscaperControl = ({ type,addLevel,removeLevel,count }) => {
+//     return ( 
+//        <div className = {classes.SkyscaperControl}>
+//             <button   className = {classes.ControlButton} onClick={()=>addLevel(type)}>+</button>
+//             <div className={classes.level}>
+//             <SkyscaperLevel type={type}  fixed />
+//             </div>
+//             <button  className = {classes.ControlButton}  onClick={()=>removeLevel(type)}   disabled={!count}  >-</button>
+//         </div>
+//     );
+// }
+//  export default SkyscaperControl;
+
+
+
+ //import Button from "../../../UI/Button/Button";
+ import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
+ import classes from "./SkyscaperControl.module.css";
+const SkyscaperControl = ({ type, addLevel, removeLevel }) => {
     return ( 
-       <div className = {classes.SkyscaperControl}>
-            <button   className = {classes.ControlButton} onClick={()=>addLevel(type)}>+</button>
-            <div className={classes.level}>
-            <SkyscaperLevel type={type}  fixed />
-            </div>
-            <button  className = {classes.ControlButton}  onClick={()=>removeLevel(type)}   disabled={!count}  >-</button>
+        <div className = {classes.SkyscaperControl}>
+            <button className = {classes.ControlButton} onClick = {() => removeLevel(type)}>-</button>
+            <SkyscaperLevel  type = {type} >
+                
+            </SkyscaperLevel>
+            <button className = {classes.ControlButton} onClick = {() => addLevel(type)}>+</button>
         </div>
     );
 }
- export default SkyscaperControl;
+export default SkyscaperControl;
--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -17,49 +17,23 @@
 //  export default SkyscaperLevel;
 
 import floor1 from "../../../Images/livingFloor.jpg"
-
 import floor2 from "../../../Images/bookstoree1.png"
 import floor3 from "../../../Images/changingroom.png"
-
-// import floor2 from "../../../Images/lh.jpg"
-// import floor3 from "../../../Images/nhomeee..jpg"
-
-
-
-
 import floor4 from "../../../Images/flower.jpg"
-
 import floor5 from "../../../Images/plants.png"
-
-
-
 const SkyscaperLevel =({type}) => {
     const types = {
         floor1 : {backgroundImage : `url(${floor1})` , width:"170px", height:"80px",  backgroundSize: "Cover"},
         floor2 : {backgroundImage : `url(${floor2})` , width:"170px", height:"100px",  backgroundSize: "Cover"},
         floor3 : {backgroundImage : `url(${floor3})` , width:"170px", height:"120px",  backgroundSize: "Cover"},
-
-
-      //  floor2 : {backgroundImage : `url(${floor2})` , width:"200px", height:"100px",  backgroundSize: "Cover"},
-      //   floor3 : {backgroundImage : `url(${floor3})` , width:"200px", height:"120px",  backgroundSize: "Cover"},
-
-
-
-
-
-
         floor4 : {
           backgroundImage : `url(${floor4})` ,
           backgroundSize: "Cover",
           width:"170px",
           display: "block",
           height:"120px"
-
         },
-
         floor5 : {backgroundImage : `url(${floor5})` , width:"170px", height:"120px",  backgroundSize: "Cover"},
-
-     
     }
     return ( 
         <div style={types[type]}></div>
