2c8442c17be1452c393a13ba7acf9e94e085d8ef
Your Name <you@example.com>
2021-04-09 00:37:47 -0700

    remove and added new styles for SControl

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
@@ -1,4 +1,28 @@
 
+// import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
+// import classes from "./SkyscaperControl.module.css";
+// const SkyscaperControl = ({ type, addLevel, removeLevel }) => {
+//     const name ={
+//         floor1: "Guest house",
+//    floor2:"library",
+//    floor3:"shopping",
+//    floor4:"flower shop",
+//    floor5:"plant house",
+//     }
+//    return ( 
+//        <div className = {classes.SkyscaperControl}>
+//            <button className = {classes.ControlButton} onClick = {() => removeLevel(type)}>-</button>
+//            {/* <SkyscaperLevel  type = {type} >
+               
+//            </SkyscaperLevel> */}
+//            {name[type]}
+//            <button className = {classes.ControlButton} >+</button>
+//        </div>
+//    );
+// }
+// export default SkyscaperControl;
+ 
+
 import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
 import classes from "./SkyscaperControl.module.css";
 const SkyscaperControl = ({ type, addLevel, removeLevel }) => {
@@ -13,7 +37,6 @@ const SkyscaperControl = ({ type, addLevel, removeLevel }) => {
        <div className = {classes.SkyscaperControl}>
            <button className = {classes.ControlButton} onClick = {() => removeLevel(type)}>-</button>
            {/* <SkyscaperLevel  type = {type} >
-               
            </SkyscaperLevel> */}
            {name[type]}
            <button className = {classes.ControlButton} onClick = {() => addLevel(type)}>+</button>
