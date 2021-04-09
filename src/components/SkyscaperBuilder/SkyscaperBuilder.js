f8ea41d774275c8daf925286c4db7137f3e6a290
Your Name <you@example.com>
2021-04-09 01:53:26 -0700

    dded order summary

--- src/components/SkyscaperBuilder/SkyscaperBuilder.js
+++ src/components/SkyscaperBuilder/SkyscaperBuilder.js
@@ -4,44 +4,46 @@ import SkyscaperPreview from "./SkyscaperPreview/SkyscaperPreview";
 import SkyscaperControls from "./SkyscaperControls/SkyscaperControls";
 import axios from "axios"
 import Modal from "../UI/Modal/Modal";
-import { useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 import OrderSummary from "./OrderSummary/OrderSummary";
 import Button from "../UI/Button/Button"
+const SkyscaperBuilder = () => {
+  const prices = {
+    floor1: 12000,
+    floor2: 1234567,
+    floor3: 12345,
+    floor4: 34567,
+    floor5: 34567,
+  }
+  const [levels, setLevels] = useState({});
+  const [price, setPrice] = useState(150);
+  const [ordering, setOrdering] = useState(false);
 
+  useEffect(loadDefaults, []);
 
-const  SkyscaperBuilder = () => {
-    const prices ={ 
-   floor1: 12000,
-   floor2:1234567,
-   floor3:12345,
-   floor4:34567,
-   floor5:34567,
+  function loadDefaults() {
+    axios
+      .get('https://builder-6b86c-default-rtdb.firebaseio.com/default.json')
+      .then(response => {
+        setPrice(response.data.price);
+        setLevels(response.data.levels);
+      });
+  }
+
+  function addLevel(type) {
+    const newlevels = { ...levels };
+    newlevels[type]++;
+    setPrice(price + prices[type]);
+    setLevels(newlevels);
+  }
+  function removeLevel(type) {
+    if (levels[type]) {
+      const newLevels = { ...levels };
+      newLevels[type]--;
+      setLevels(newLevels);
+      setPrice(price - prices[type])
     }
-    const [levels, setLevels]= useState({ });
-    const [price, setPrice] = useState(150);
-    const [ordering, setOrdering] = useState(false);
-        useEffect ( 
-            () => axios
-            .get('https://builder-6b86c-default-rtdb.firebaseio.com/default.json')
-                .then(response=>{
-                setLevels(response.data.levels);
-                setPrice(response.data.price);
-              }),[]
-          );
-    function addLevel(type){
-        const newlevels={...levels};
-        newlevels[type]++;
-        setPrice(price + prices[type]);
-        setLevels(newlevels);
-    }
-    function removeLevel(type){
-        if (levels[type]) {
-            const newLevels = { ...levels };
-          newLevels[type]--;
-          setLevels(newLevels);
-          setPrice(price - prices[type])
-          }
-    }
+  }
 
   function startOrdering() {
     setOrdering(true);
@@ -50,34 +52,46 @@ const  SkyscaperBuilder = () => {
     setOrdering(false);
   }
 
+
   function finishOrdering() {
-    setOrdering(false);
+    axios
+      .post('https://builder-6b86c-default-rtdb.firebaseio.com/orders.json', {
+        levels: levels,
+        price: price,
+        address: "oktyabrskaya",
+        phone: "0 777 777 777",
+        name: "Sadyr Japarov",
+      })
+      .then(() => {
+        setOrdering(false);
+        loadDefaults();
+      });
   }
   return (
-        <div className={classes.SkyscaperBuilder}>  
-            <SkyscaperPreview  levels={levels}
-             price={price} />
-            <SkyscaperControls 
-             levels = {levels}    
-            addLevel={addLevel}
-            removeLevel={removeLevel}
-            startOrdering={startOrdering}
-            />        
+    <div className={classes.SkyscaperBuilder}>
+      <SkyscaperPreview levels={levels}
+        price={price} />
+      <SkyscaperControls
+        levels={levels}
+        addLevel={addLevel}
+        removeLevel={removeLevel}
+        startOrdering={startOrdering}
+      />
       <Modal
         show={ordering}
         cancel={stopOrdering}>Hello
         <OrderSummary
-            levels={levels}
-            price={price}
-            />
-               <Button onClick={finishOrdering} green>Checkout</Button>
-            <Button onClick={stopOrdering}>Cancel</Button>
+          levels={levels}
+          price={price}
+        />
+        <Button onClick={finishOrdering} green>Checkout</Button>
+        <Button onClick={stopOrdering}>Cancel</Button>
 
-        </Modal>
+      </Modal>
 
 
-        </div>
-    )
+    </div>
+  )
 };
 export default SkyscaperBuilder;
 
