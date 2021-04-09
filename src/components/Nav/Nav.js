2de7bb0c6f7834ea8d65538a8e9a42ae100d3f4a
Your Name <you@example.com>
2021-04-09 02:28:46 -0700

    added new functions for builder

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,11 @@
 import classes from "./Nav.module.css";
-
+import NavItem from "./NavItem/NavItem";
 const Nav = () => {
     return (
-        <div className={classes.Nav}>
-       Nav
-
-        </div>
+        <ul className={classes.Nav}>
+         <NavItem url="/" active>Builder</NavItem>
+      <NavItem url="/">Checkout</NavItem>
+        </ul>
     )
 }
 
