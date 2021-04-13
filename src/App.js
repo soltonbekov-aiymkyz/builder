a1374701ad7ecb326114c87fcbdb7a430a370357
Your Name <you@example.com>
2021-04-13 02:26:36 -0700

    added some functions

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import SkyscaperBuilder from "./components/SkyscaperBuilder/SkyscaperBuilder";
 
+// import React, { Component } from 'react';
+// import styled, { keyframes } from 'styled-components';
+// import { bounce } from 'react-animations';
+
+
+
+// const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`
+
 function App() {
   return (
     <div className="App">
      <Layout>
        <h1>Skyscaper builder by Soltonbekova Aiym</h1>
+       {/* <Bounce><h1>Hello Animation Bounce</h1></Bounce> */}
+       
+
       <SkyscaperBuilder />
      </Layout>
     </div>
