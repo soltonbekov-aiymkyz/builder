6875af086bed149eb040cd664e57a0ca77eb1975
Your Name <you@example.com>
2021-04-13 04:18:42 -0700

    create animation in app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,16 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import SkyscaperBuilder from "./components/SkyscaperBuilder/SkyscaperBuilder";
 
-// import React, { Component } from 'react';
-// import styled, { keyframes } from 'styled-components';
-// import { bounce } from 'react-animations';
-
-
-
-// const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`
 
 function App() {
   return (
     <div className="App">
      <Layout>
-       <h1>Skyscaper builder </h1>
-       {/* <Bounce><h1>Hello Animation Bounce</h1></Bounce> */}
+       <h1>Skyscaper builder by Soltonbekova Aiym </h1>
       <SkyscaperBuilder />
      </Layout>
     </div>
   );
 }
 export default App;
+
