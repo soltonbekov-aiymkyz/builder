eff469698dab702fbe2fefe18aad36ff6df55824
Your Name <you@example.com>
2021-04-16 00:06:47 -0700

    added new animation and function

--- src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
+++ src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
@@ -1,4 +1,3 @@
-
 import classes from "./SkyscaperPreview.module.css";
 import SkyscaperLevel from "../SkyscaperLevel/SkyscaperLevel";
 
@@ -9,36 +8,13 @@ for (const level in levels){
     result.push(<SkyscaperLevel  key={level + i} type={level}/>)
 }
  }
-
-
-// function loop(x) {
-//     if (x >= 10) // "x >= 10" — это условие для конца выполнения (тоже самое, что "!(x < 10)")
-//       return;
-//     // делать что-то
-//     loop(x + 1); // рекурсионный вызов
-//   }
-//   loop(0);
-
-
-
-//   function animation({level}) {
-//     if (level == 10) 
-//       return;
-//     animation(level + 1);
-//   }
-//   animation(0);
-
-
-
-
-
-
 return ( 
     <div className = {classes.SkyscaperPreview}>
-    
         <div className={classes.levels}>
             <div
             className ={classes.levels}/>
+            <div className={classes.birdImage}>
+            </div>
             {result}
         </div>
     <div className={classes.price}>{price.toFixed(1)}som</div>
