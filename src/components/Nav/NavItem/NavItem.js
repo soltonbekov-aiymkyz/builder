2e6c16a6395724add85e430cf4bc8ad7aa83b05c
Your Name <you@example.com>
2021-04-16 23:16:29 -0700

    added new style

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,10 +1,15 @@
+
+import { NavLink } from "react-router-dom";
 import classes from "./NavItem.module.css";
-const NavItem = ({ url, children, active }) => {
+const NavItem = ({ url, children, exact }) => {
   return (
     <li className={classes.NavItem}>
-      <a href={url} className={active ? classes.active : null}>{children}</a>
+      
+      <NavLink to={url} activeClassName={classes.active} exact>{children}</NavLink>
     </li>
   );
 }
 
 export default NavItem;
+
+
