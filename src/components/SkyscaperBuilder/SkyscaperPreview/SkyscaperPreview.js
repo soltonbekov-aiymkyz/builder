4b19dcaea2114528d56f6c78812498a39116f0d4
Your Name <you@example.com>
2021-04-18 07:29:05 -0700

    added new styles for this page

--- src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
+++ src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
@@ -1,29 +1,30 @@
+
+
+import React from "react";
 import classes from "./SkyscaperPreview.module.css";
 import SkyscaperLevel from "../SkyscaperLevel/SkyscaperLevel";
-
 const  SkyscaperPreview = ({levels,price}) => {
     const result = [];
 for (const level in levels){
     for (let i=0; i<levels[level]; i++){
     result.push(<SkyscaperLevel  key={level + i} type={level}/>)
 }
-
  }
+
  
+
+    
 return ( 
     <div className = {classes.SkyscaperPreview}>
-        <div className={classes.levels}>
-            <div
-            className ={classes.levels}/>
-            <div className={classes.birdImage}>
-                
-            </div>
+        <div className={classes.levels}> 
+             <div className ={classes.levels}/> 
+             <div className={classes.birdImage}></div>
             {result}
         </div>
     <div className={classes.price}>{price.toFixed(1)}som</div>
     </div>
 );}
 export default SkyscaperPreview;
- 
- 
 
+  
+  
