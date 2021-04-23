8cbc2dfdd0f4109f50cf6f479aae5549f1cbfc0c
Your Name <you@example.com>
2021-04-22 22:12:45 -0700

    changet some funcruions

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -9,8 +9,8 @@ const CheckoutSummary = ({ cancelCallback }) => {
         <SkyscaperPreview levels={{
           floor1: 5, 
           floor2: 10,
-          floor3: 50,
-        }} price={150} />
+          floor3: 5,
+        }} price={12000} />
       </div>
       <div>
         <Button>Checkout</Button>
@@ -21,3 +21,32 @@ const CheckoutSummary = ({ cancelCallback }) => {
 }
  
 export default CheckoutSummary;
+
+
+
+
+
+
+// import classes from "./CheckoutSummary.module.css";
+// import SkyscaperPreview from "../../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
+// import Button from "../../UI/Button/Button";
+
+// const CheckoutSummary = ({ cancelCallback }) => {
+//   return (
+//     <div className={classes.CheckoutSummary}>
+//       <div>
+//         <SkyscaperPreview pots={{
+//             floor1: 5, 
+//                       floor2: 10,
+//                       floor3: 5,
+//         }} price={150} />
+//       </div>
+//       <div>
+//         <Button>Checkout</Button>
+//         <Button onClick={cancelCallback}>Cancel</Button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default CheckoutSummary; 
