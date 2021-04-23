58bfeb2491e67f5ac2c926682537d84203344b66
Your Name <you@example.com>
2021-04-22 22:20:40 -0700

    added some functions

--- src/components/SkyscaperBuilder/OrderSummary/OrderSummary.js
+++ src/components/SkyscaperBuilder/OrderSummary/OrderSummary.js
@@ -1,22 +1,49 @@
+// import classes from "./OrderSummary.module.css";
+// const OrderSummary = ({ levels, price }) => {
+//   const labels = {
+//     floor1: "Guest house",
+//    floor2:"library",
+//    floor3:"shopping",
+//    floor4:"flower shop",
+//    floor5:"plant house",
+//   }
+//   const results = Object.keys(levels)
+//     .map(type => <li>{labels[type]}: {levels[type]}</li>);
+//   return (
+//     <div className={classes.OrderSummary}>
+//       <h3>Order summary</h3>
+//       <ul>
+//         {results}
+//       </ul>
+//       <strong>Total price: {price.toFixed(1)} som</strong>
+//     </div>
+//   );
+// }
+// export default OrderSummary;
+
+
+
 import classes from "./OrderSummary.module.css";
-const OrderSummary = ({ levels, price }) => {
-  const labels = {
-    floor1: "Guest house",
-   floor2:"library",
-   floor3:"shopping",
-   floor4:"flower shop",
-   floor5:"plant house",
-  }
-  const results = Object.keys(levels)
-    .map(type => <li>{labels[type]}: {levels[type]}</li>);
-  return (
-    <div className={classes.OrderSummary}>
-      <h3>Order summary</h3>
-      <ul>
-        {results}
-      </ul>
-      <strong>Total price: {price.toFixed(1)} som</strong>
-    </div>
-  );
+
+const OrderSummary = ({levels, price}) => {
+    const labels = {
+      floor1: "Guest house",
+         floor2:"library",
+         floor3:"shopping",
+         floor4:"flower shop",
+         floor5:"plant house",
+    }
+    const results =Object.keys(levels)
+     .map(type => <li>{labels[type]} : {levels[type]}</li>)
+    return ( 
+        <div className={classes.OrderSummary}>
+            <h3>Order Summary:</h3>
+            <ul>
+                {results}
+            </ul>
+            <strong>Total price: {price.toFixed(1)} som</strong>
+        </div>   
+     );
 }
+ 
 export default OrderSummary;
