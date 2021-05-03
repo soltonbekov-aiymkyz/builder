71caef4e5d0350f52423f295d125a9574747eea6
Your Name <you@example.com>
2021-05-03 02:15:10 -0700

    delete some functions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
+// import "./App.css";
+// import Layout from "./components/Layout/Layout";
+// import SkyscaperBuilder from "./components/SkyscaperBuilder/SkyscaperBuilder";
+// import Checkout from "./components/Checkout/Checkout"
+// import { Redirect, Route, Switch } from "react-router";
+// function App() {
+//   return (
+//     <div className="App">
+//      <Layout>
+//      <Switch>
+//           <Route path="/" component={SkyscaperBuilder} exact />
+//           <Route path="/checkout" component={Checkout} />
+//           <Redirect to="/" />
+//         </Switch>
+//        <h1>Skyscaper builder by Soltonbekova Aiym </h1>
+//      </Layout>
+//     </div>
+//   );
+// }
+// export default App;
 
-import "./App.css";
 import Layout from "./components/Layout/Layout";
 import SkyscaperBuilder from "./components/SkyscaperBuilder/SkyscaperBuilder";
-import Checkout from "./components/Checkout/Checkout"
+import Checkout from "./components/Checkout/Checkout";
+import Orders from "./components/Orders/Orders";
+import "./App.css";
 import { Redirect, Route, Switch } from "react-router";
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-     <Layout>
-     <Switch>
+      <Layout>
+        <Switch>
           <Route path="/" component={SkyscaperBuilder} exact />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Redirect to="/" />
         </Switch>
-       <h1>Skyscaper builder by Soltonbekova Aiym </h1>
-     </Layout>
+      </Layout>
     </div>
   );
 }
+
 export default App;
-
-
-
-
 
 
 
