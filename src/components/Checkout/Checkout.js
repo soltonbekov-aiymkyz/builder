1425263433d232863bdb76585b8d803f0a887129
Your Name <you@example.com>
2021-05-03 02:15:26 -0700

    added new file odred

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,28 +1,101 @@
 
-//   import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
+// import CheckoutSummary from "./CheckoutSummary/CheckoutSummary";
+// const Checkout = ({ history }) => {
+//     function cancelCallback() {
+//       history.replace('/');
+//     }
+//     return (
+//         <div>
+//           <CheckoutSummary cancelCallback={cancelCallback} />
+//         </div>
+//       );
+//     }
+
+// export default Checkout;
+
+
+// import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
+// import CheckoutForm from "./CheckoutForm/CheckoutForm";
 // const Checkout = ({ history }) => {
 //   function cancelCallback() {
 //     history.replace('/');
 //   }
 //   return (
 //     <div>
-//       <CheckoutSummary cancelCallback={cancelCallback} />
+//       <SkyscaperPreview levels={{
+//           floor1: 5, 
+//           floor2: 10,
+//           floor3: 5,
+//         }} price={12000} />
+//       <CheckoutForm />
 //     </div>
 //   );
 // }
 // export default Checkout;
 
+// import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
+// import CheckoutForm from "./CheckoutForm/CheckoutForm";
+// import classes from "./Checkout.module.css";
+// const Checkout = ({ history }) => {
+//   function cancelCallback() {
+//     history.replace('/');
+//   }
+//   return (
+//     <div className={classes.Checkout}>
+//      <SkyscaperPreview levels={{
+//           floor1: 5, 
+//           floor2: 10,
+//           floor3: 5,
+//         }} price={12000} />
+//       <CheckoutForm 
+//       cancelCallback={cancelCallback}
 
-import CheckoutSummary from "./CheckoutSummary/CheckoutSummary";
+//       />
+//     </div>
+//   );
+// }
+ 
+// export default Checkout;
+
+
+import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
+import CheckoutForm from "./CheckoutForm/CheckoutForm";
+import classes from "./Checkout.module.css";
+import axios from "axios";
 const Checkout = ({ history }) => {
-    function cancelCallback() {
+  function cancelCallback() {
+    history.replace('/');
+  }
+  function submitCallback(event) {
+    const data = new FormData(event.target);
+    axios.post('https://builder-6b86c-default-rtdb.firebaseio.com/orders.json', {
+      name: data.get('name'),
+      address: data.get('address'),
+      phone: data.get('phone'),
+      levels: {
+        floo1: 10,
+        floor2: 10,
+        floor3: 10,
+        floor4: 10,
+        floor5: 10,
+      },
+      price: 100,
+    }).then(response => {
       history.replace('/');
-    }
-    return (
-        <div>
-          <CheckoutSummary cancelCallback={cancelCallback} />
-        </div>
-      );
-    }
-
+    });
+    event.preventDefault();
+  }
+  return (
+    <div className={classes.Checkout}>
+      <SkyscaperPreview levels={{
+        floor1: 5, 
+        floor2: 10,
+       floor3: 50,
+      }} price={150} />
+      <CheckoutForm
+        cancelCallback={cancelCallback}
+        submitCallback={submitCallback} />
+    </div>
+  );
+}
 export default Checkout;
