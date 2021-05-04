11452cd4a76226db9f127c051e8c5c34bf797f1a
Your Name <you@example.com>
2021-05-04 02:27:15 -0700

    change nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,27 @@
+// import classes from "./Nav.module.css";
+// import NavItem from "./NavItem/NavItem";
+// const Nav = () => {
+//     return (
+//         <ul className={classes.Nav}>
+//          <NavItem url="/" exact>Builder</NavItem>
+//       <NavItem url="/checkout">Checkout</NavItem>
+//         </ul>
+//     )
+// }
+
+// export default Nav;
+
+
+
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
 const Nav = () => {
-    return (
-        <ul className={classes.Nav}>
-         <NavItem url="/" exact>Builder</NavItem>
-      <NavItem url="/checkout">Checkout</NavItem>
-        </ul>
-    )
+  return (
+    <ul className={classes.Nav}>
+      <NavItem url="/" exact>Builder</NavItem>
+      <NavItem url="/orders">Orders</NavItem>
+    </ul>
+  );
 }
-
 export default Nav;
+
