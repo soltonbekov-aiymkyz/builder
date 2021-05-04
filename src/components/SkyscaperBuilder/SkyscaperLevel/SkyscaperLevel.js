de8e00faf4950d8c6405de7296d6088c22d9aa1b
Your Name <you@example.com>
2021-05-04 02:32:22 -0700

    corrected all mistakes in this page

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -1,9 +1,14 @@
-import React from "react"
+// // import React from "react"
+// import classes from "./SkyscaperLevel.module.css"
+
 import floor1 from "../../../Images/livingFloor.jpg"
 import floor2 from "../../../Images/me.jpg"
 import floor3 from "../../../Images/bookstoree1.png"
-import floor4 from "../../../Images/achyk.jpg"
-import floor5 from "../../../Images/changingroom.png"
+import floor4 from "../../../Images/rooos.jpg"
+import floor5 from "../../../Images/wer.jpg"
+
+// import floorF1 from "../../../Images/roof.jpg"
+
 const SkyscaperLevel =({type}) => {
     const types = {
         floor1 : {backgroundImage : `url(${floor1})` , width:"170px", height:"80px",  backgroundSize: "Cover"},
@@ -11,6 +16,10 @@ const SkyscaperLevel =({type}) => {
         floor3 : {backgroundImage : `url(${floor3})` , width:"170px", height:"120px",  backgroundSize: "Cover"},
         floor4 : {backgroundImage : `url(${floor4})`,backgroundSize:"Cover",width:"170px",/*display: "block",*/height:"120px"},
         floor5 : {backgroundImage : `url(${floor5})` , width:"170px", height:"120px",  backgroundSize: "Cover"},
+
+
+        // floorF1 : {backgroundImage : `url(${floorF1})` , width:"170px", height:"80px",  backgroundSize: "Cover"},
+
     }
     return ( 
     <div    style={types[type]}> </div>
