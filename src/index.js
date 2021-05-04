ab79ab1c0ddc9cc573f2472278675e6e725a394c
Your Name <you@example.com>
2021-05-04 02:26:09 -0700

    corrected some inport

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
 import reportWebVitals from './reportWebVitals';
+import builderReducer from './store/builderReducer';
+
+const store = createStore(builderReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+     <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
 
