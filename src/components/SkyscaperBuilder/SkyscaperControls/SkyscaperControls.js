0ef12a5e7ef52662d7c43027238ad5b46018e21c
Your Name <you@example.com>
2021-05-04 02:29:59 -0700

    change some functions

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
@@ -1,26 +1,19 @@
 
-import Button from "../../UI/Button/Button";
+ import Button from "../../UI/Button/Button";
 import SkyscaperControl from "./SkyscaperControl/SkyscaperControl";
-import classes from "./SkyscaperControls.module.css";
+ import classes from "./SkyscaperControls.module.css";
 const SkyscaperControls = ({ 
   levels,
-   addLevel, 
-   removeLevel,
    startOrdering
   }) => {
   const results = [];
   let total = 0;
   for (const level in levels) {
   total+= levels[level];
-    results.push(
-      <SkyscaperControl
+    results.push( <SkyscaperControl
         type={level}
-        addLevel={addLevel}
-        removeLevel={removeLevel}
         key={level}
-             count={level[level]}
-      />
-    )
+             count={levels[level]}   /> )
   }
 return (
   <div className={classes.SkyscaperControls}>  
@@ -35,3 +28,74 @@ export default SkyscaperControls;
 
 
 
+
+
+
+
+
+
+  
+
+// import Button from "../../UI/Button/Button";
+// import SkyscaperControl from "./SkyscaperControl/SkyscaperControl";
+// import classes from "./SkyscaperControls.module.css";
+// const SkyscaperControls = ({ 
+//   levels,
+//    addLevel, 
+//    removeLevel,
+//    startOrdering
+//   }) => {
+//   const results = [];
+//   let total = 0;
+//   for (const level in levels) {
+//   total+= levels[level];
+//     results.push(
+//       <SkyscaperControl
+//         type={level}
+//         addLevel={addLevel}
+//         removeLevel={removeLevel}
+//         key={level}
+//              count={level[level]}
+//       />
+//     )
+//   }
+// return (
+//   <div className={classes.SkyscaperControls}>  
+//   <strong>Levels</strong>
+//   {results}
+//   <Button disabled={!total} onClick={startOrdering}>Order</Button>
+//   </div>
+// );
+// };
+// export default SkyscaperControls;
+
+
+
+
+
+// import Button from "../../UI/Button/Button";
+// import SkyscaperControl from "./SkyscaperControl/SkyscaperControl";
+// import classes from "./SkyscaperControls.module.css";
+// const SkyscaperControls = ({ 
+//   levels,
+//    startOrdering
+//   }) => {
+//   const results = [];
+//   let total = 0;
+//   for (const level in levels) {
+//   total+= levels[level];
+//     results.push( <SkyscaperControl
+//         type={level}
+        
+//         key={level}
+//              count={level[level]} /> )
+//   }
+// return (
+//   <div className={classes.SkyscaperControls}>  
+//   <strong>Levels</strong>
+//   {results}
+//   <Button disabled={!total} onClick={startOrdering}>Order</Button>
+//   </div>
+// );
+// };
+// export default SkyscaperControls;
