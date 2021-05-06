ef0c5ee73082a31ad7a007e43fd9a575a5156219
Your Name <you@example.com>
2021-05-06 10:07:03 -0700

    added new folder

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -58,46 +58,88 @@
 // export default Checkout;
 
 
+// import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
+// import CheckoutForm from "./CheckoutForm/CheckoutForm";
+// import classes from "./Checkout.module.css";
+// import axios from "axios";
+// const Checkout = ({ history }) => {
+//   function cancelCallback() {
+//     history.replace('/');
+//   }
+//   function submitCallback(event) {
+//     const data = new FormData(event.target);
+//     axios.post('https://builder-6b86c-default-rtdb.firebaseio.com/orders.json', {
+//       name: data.get('name'),
+//       address: data.get('address'),
+//       phone: data.get('phone'),
+//       levels: {
+//         floo1: 3,
+//         floor2: 3,
+//         floor3: 3,
+//         floor4: 3,
+//         floor5: 3,
+//       },
+//       price: 100,
+//     }).then(response => {
+//       history.replace('/');
+//     });
+//     event.preventDefault();
+//   }
+//   return (
+//     <div className={classes.Checkout}>
+//       <SkyscaperPreview levels={{
+//         floor1: 5, 
+//         floor2: 10,
+//        floor3: 5,
+//       }} price={150} />
+//       <CheckoutForm
+//         cancelCallback={cancelCallback}
+//         submitCallback={submitCallback} />
+//     </div>
+//   );
+// }
+// export default Checkout;
+
+  
 import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import classes from "./Checkout.module.css";
 import axios from "axios";
+import { useSelector } from "react-redux";
 const Checkout = ({ history }) => {
+  const levels = useSelector(state => state.levels);
+  const price = useSelector(state => state.price);
+
   function cancelCallback() {
     history.replace('/');
   }
+
   function submitCallback(event) {
     const data = new FormData(event.target);
+
     axios.post('https://builder-6b86c-default-rtdb.firebaseio.com/orders.json', {
       name: data.get('name'),
       address: data.get('address'),
       phone: data.get('phone'),
-      levels: {
-        floo1: 3,
-        floor2: 3,
-        floor3: 3,
-        floor4: 3,
-        floor5: 3,
-      },
-      price: 100,
+      levels: levels,
+      price: price,
     }).then(response => {
       history.replace('/');
     });
+
     event.preventDefault();
   }
+
   return (
     <div className={classes.Checkout}>
-      <SkyscaperPreview levels={{
-        floor1: 5, 
-        floor2: 10,
-       floor3: 5,
-      }} price={150} />
+      <SkyscaperPreview levels={levels} price={price} />
       <CheckoutForm
         cancelCallback={cancelCallback}
         submitCallback={submitCallback} />
     </div>
   );
 }
+ 
 export default Checkout;
 
 
