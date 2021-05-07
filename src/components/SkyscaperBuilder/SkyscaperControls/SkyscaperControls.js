6a818a763d8e2f455fb7da68206b4b798a43b5a5
Your Name <you@example.com>
2021-05-07 02:32:06 -0700

    adde3d some functions

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
@@ -28,8 +28,44 @@ export default SkyscaperControls;
 
 
 
-
-
+  
+// import Button from "../../UI/Button/Button";
+// import SkyscaperControl from "./SkyscaperControl/SkyscaperControl";
+// import classes from "./SkyscaperControls.module.css";
+// import SwitchSkyscaper from "./SwitchSkyscaper/SwitchSkyscaper";
+// const SkyscaperControls = ({
+//   switchFilling,
+//   startOrdering,
+//   filling,
+//   levels,
+// }) => {
+//   const result = [];
+//   let total = 0;
+//   for (const level in levels) {
+//     total += levels[level];
+//     result.push(
+//       <SkyscaperControl
+//         type={level}
+//         key={level}
+//         filling={filling}
+//         switchFilling={switchFilling}
+//         count={levels[level]}
+//       />
+//     );
+//   }
+//   return (
+//     <div className={classes.SkyscaperControls}>
+//       <SwitchSkyscaper switchFilling={switchFilling} />
+//       {result}
+//       <div className={classes.DivButton}>
+//         <Button disabled={!total} onClick={startOrdering} glav="true">
+//           Order
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+// export default SkyscaperControls;
 
 
 
