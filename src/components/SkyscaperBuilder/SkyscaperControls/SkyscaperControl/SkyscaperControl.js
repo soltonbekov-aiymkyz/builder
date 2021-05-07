6b2877e5d9a879201211f045b51f4ece8e23447a
Your Name <you@example.com>
2021-05-07 02:34:24 -0700

    addeed some functions

--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControl/SkyscaperControl.js
@@ -1,29 +1,98 @@
-import { useDispatch } from "react-redux";
-import Button from "../../../UI/Button/Button";
- import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
-import classes from "./SkyscaperControl.module.css";
-const SkyscaperControl = ({ type, count }) => {
-    const dispatch = useDispatch();
+// import { useDispatch } from "react-redux";
+// import Button from "../../../UI/Button/Button";
+//  import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
+// import classes from "./SkyscaperControl.module.css";
+// const SkyscaperControl = ({ type, count }) => {
+//     const dispatch = useDispatch();
 
-    const name ={
-                floor1: "Guest house",
-           floor2:"library",
-           floor3:"shopping",
-           floor4:"flower shop",
-           floor5:"plant house",
-            }
-return (
+//     const name ={
+//                 floor1: "Guest house",
+//            floor2:"library",
+//            floor3:"shopping",
+//            floor4:"flower shop",
+//            floor5:"plant house",
+//             }
+// return (
+//     <div className={classes.SkyscaperControl}>
+//       <Button onClick={() => dispatch({ type: "ADD_LEVEL", level: type })}>+</Button>
+//       <div className={classes.level}>
+//         {/* <SkyscaperLevel type={type} fixed /> */}
+//         {name[type]}
+//       </div>
+//       <Button onClick={() => dispatch({ type: "REMOVE_LEVEL", level: type })} disabled={!count}>-</Button>
+//     </div>
+//   );
+// }
+// export default SkyscaperControl;
+
+import { useDispatch } from "react-redux";
+import { add, remove } from "../../../../store/actions/builder";
+import Button from "../../../UI/Button/Button";
+import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
+import classes from "./SkyscaperControl.module.css";
+
+const SkyscaperControl = ({ type, count }) => {
+  const dispatch = useDispatch();
+
+  // const name ={
+  //                   floor1: "Guest house",
+  //              floor2:"library",
+  //              floor3:"shopping",
+  //              floor4:"flower shop",
+  //              floor5:"plant house",
+  //               }
+
+  return (
     <div className={classes.SkyscaperControl}>
-      <Button onClick={() => dispatch({ type: "ADD_LEVEL", level: type })}>+</Button>
+      <Button onClick={() => dispatch(add(type))}>+</Button>
       <div className={classes.level}>
-        {/* <SkyscaperLevel type={type} fixed /> */}
-        {name[type]}
+        <SkyscaperLevel type={type} fixed />
       </div>
-      <Button onClick={() => dispatch({ type: "REMOVE_LEVEL", level: type })} disabled={!count}>-</Button>
+      <Button onClick={() => dispatch(remove(type))} disabled={!count}>-</Button>
     </div>
   );
 }
+
 export default SkyscaperControl;
+
+
+  
+// import { useDispatch } from "react-redux";
+// import Button from "../../../UI/Button/Button";
+// import SkyscaperLevel from "../../SkyscaperLevel/SkyscaperLevel";
+// import classes from "./SkyscaperControl.module.css";
+// const SkyscaperControl = ({ count, type, filling }) => {
+//     const dispatch = useDispatch();
+//     const name ={
+//  floor1: "Guest house",
+//  floor2:"library",
+//  floor3:"shopping",
+//  floor4:"flower shop",
+//  floor5:"plant house",
+//   }
+//     return ( 
+//         <div className = {classes.SkyscaperControl}>
+//             <Button className = {classes.ControlButton} onClick = {() => dispatch({ type: "REMOVE_LEVEL", level: type })} disabled={!count}>-</Button>
+//             <div className={classes.level}>
+//                 {/* <SkyscaperLevel type = {type + filling} fixed /> */}
+//                 {name[type]}
+//             </div>
+//             <Button className = {classes.ControlButton} onClick = {() => dispatch({ type: "ADD_LEVEL", level: type })}>+</Button>
+//         </div>
+//     );
+// }
+// export default SkyscaperControl;
+
+
+
+
+
+
+
+
+
+
+
 
 
 // import classes from "./SkyscaperControl.module.css";
