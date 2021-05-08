09ba12a037cc4bdc9b5c1305649b707d1d81eb9a
Your Name <you@example.com>
2021-05-08 06:39:26 -0700

    create and delete with axios file

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,107 +1,4 @@
-
-// import CheckoutSummary from "./CheckoutSummary/CheckoutSummary";
-// const Checkout = ({ history }) => {
-//     function cancelCallback() {
-//       history.replace('/');
-//     }
-//     return (
-//         <div>
-//           <CheckoutSummary cancelCallback={cancelCallback} />
-//         </div>
-//       );
-//     }
-
-// export default Checkout;
-
-
-// import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
-// import CheckoutForm from "./CheckoutForm/CheckoutForm";
-// const Checkout = ({ history }) => {
-//   function cancelCallback() {
-//     history.replace('/');
-//   }
-//   return (
-//     <div>
-//       <SkyscaperPreview levels={{
-//           floor1: 5, 
-//           floor2: 10,
-//           floor3: 5,
-//         }} price={12000} />
-//       <CheckoutForm />
-//     </div>
-//   );
-// }
-// export default Checkout;
-
-// import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
-// import CheckoutForm from "./CheckoutForm/CheckoutForm";
-// import classes from "./Checkout.module.css";
-// const Checkout = ({ history }) => {
-//   function cancelCallback() {
-//     history.replace('/');
-//   }
-//   return (
-//     <div className={classes.Checkout}>
-//      <SkyscaperPreview levels={{
-//           floor1: 5, 
-//           floor2: 10,
-//           floor3: 5,
-//         }} price={12000} />
-//       <CheckoutForm 
-//       cancelCallback={cancelCallback}
-
-//       />
-//     </div>
-//   );
-// }
- 
-// export default Checkout;
-
-
-// import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
-// import CheckoutForm from "./CheckoutForm/CheckoutForm";
-// import classes from "./Checkout.module.css";
-// import axios from "axios";
-// const Checkout = ({ history }) => {
-//   function cancelCallback() {
-//     history.replace('/');
-//   }
-//   function submitCallback(event) {
-//     const data = new FormData(event.target);
-//     axios.post('https://builder-6b86c-default-rtdb.firebaseio.com/orders.json', {
-//       name: data.get('name'),
-//       address: data.get('address'),
-//       phone: data.get('phone'),
-//       levels: {
-//         floo1: 3,
-//         floor2: 3,
-//         floor3: 3,
-//         floor4: 3,
-//         floor5: 3,
-//       },
-//       price: 100,
-//     }).then(response => {
-//       history.replace('/');
-//     });
-//     event.preventDefault();
-//   }
-//   return (
-//     <div className={classes.Checkout}>
-//       <SkyscaperPreview levels={{
-//         floor1: 5, 
-//         floor2: 10,
-//        floor3: 5,
-//       }} price={150} />
-//       <CheckoutForm
-//         cancelCallback={cancelCallback}
-//         submitCallback={submitCallback} />
-//     </div>
-//   );
-// }
-// export default Checkout;
-
-  
-import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
+ import SkyscaperPreview from "../SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import classes from "./Checkout.module.css";
 import axios from "axios";
@@ -109,15 +6,12 @@ import { useSelector } from "react-redux";
 const Checkout = ({ history }) => {
   const levels = useSelector(state => state.builder.levels);
   const price = useSelector(state => state.builder.price);
-
   function cancelCallback() {
     history.replace('/');
   }
-
   function submitCallback(event) {
     const data = new FormData(event.target);
-
-    axios.post('https://builder-6b86c-default-rtdb.firebaseio.com/orders.json', {
+    axios.post('/orders.json', {
       name: data.get('name'),
       address: data.get('address'),
       phone: data.get('phone'),
@@ -129,7 +23,6 @@ const Checkout = ({ history }) => {
 
     event.preventDefault();
   }
-
   return (
     <div className={classes.Checkout}>
       <SkyscaperPreview levels={levels} price={price} />
@@ -139,7 +32,6 @@ const Checkout = ({ history }) => {
     </div>
   );
 }
- 
 export default Checkout;
 
 
@@ -147,10 +39,4 @@ export default Checkout;
 
 
 
-
-
-
-
-
-
-
+  
