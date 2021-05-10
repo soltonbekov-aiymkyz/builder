1b79c442a1fc1a8966d016cd86a257ff0453cc3f
Your Name <you@example.com>
2021-05-10 01:07:08 -0700

    delete osme pages sk

--- src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
+++ src/components/SkyscaperBuilder/SkyscaperLevel/SkyscaperLevel.js
@@ -1,6 +1,5 @@
 
-
-
+// import floor0 from "../../../Images/gym20.jpg"
 import floor1 from "../../../Images/livingFloor.jpg"
 import floor2 from "../../../Images/do.jpg"
 import floor3 from "../../../Images/glob.jpg"
@@ -8,22 +7,15 @@ import floor4 from "../../../Images/bookstoree1.png"
 import floor5 from "../../../Images/sale.jpg"
 
 
-
-// import floor6 from "../../../Images/gym.jpg"
-
 const SkyscaperLevel = ({ type }) => {
     const types = {
 
+        // floor0: { backgroundImage: `url(${floor0})`, width: "170px", height: "80px", backgroundSize: "Cover" },
         floor1: { backgroundImage: `url(${floor1})`, width: "170px", height: "80px", backgroundSize: "Cover" },
         floor2: { backgroundImage: `url(${floor2})`, width: "170px", height: "100px", backgroundSize: "Cover" },
         floor3: { backgroundImage: `url(${floor3})`, width: "170px", height: "120px", backgroundSize: "Cover" },
         floor4: { backgroundImage: `url(${floor4})`, backgroundSize: "Cover", width: "170px", height: "100px" },
         floor5: { backgroundImage: `url(${floor5})`, width: "170px", height: "110px", backgroundSize: "Cover" },
-   
-
-        // floor6: { backgroundImage: `url(${floor1})`, width: "170px", height: "80px", backgroundSize: "Cover" },
-
-   
     }
     return (
         <div style={types[type]}> </div>
