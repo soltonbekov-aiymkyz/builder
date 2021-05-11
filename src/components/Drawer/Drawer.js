37dfd54db5b44ed7e49c2bd380889d5c9633617f
Your Name <you@example.com>
2021-05-10 20:54:41 -0700

    delete indents in drawer js

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,33 +1,8 @@
-// import classes from "./Drawer.module.css";
-// import Nav from "../Nav/Nav"
-// import Logo from "../Logo/Logo"
-// import Backdrop from "../UI/Backdrop/Backdrop";
-
-//    const Drawer=()=>{[drawerOpen,setDrawerOpen] = useState(false)
-
-//         const drawerClasses =[
-//             classes.content,
-//             class
-//         ]
-//     }
-
-//     return (
-//         <div className={classes.Drawer}>
-//             <Backdrop show={drawerOpen} />
-//             <div className={[classes.content, classes.close].join(" ")}>
-//         <Logo />
-//         <Nav />
-//         </div>
-//     </div>
-//     )
-// }
-// export default Drawer;
 
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import Backdrop from "../UI/Backdrop/Backdrop";
 import classes from "./Drawer.module.css";
-
 const Drawer = ({ open, closeDrawer }) => {
   const drawerClasses = [
     classes.content,
