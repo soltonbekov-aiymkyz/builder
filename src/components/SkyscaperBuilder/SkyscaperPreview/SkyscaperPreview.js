ef7815b1b6156952ce0bb0f8d6aae5befbb63791
Your Name <you@example.com>
2021-05-10 20:56:41 -0700

    delete indent in this page

--- src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
+++ src/components/SkyscaperBuilder/SkyscaperPreview/SkyscaperPreview.js
@@ -8,23 +8,42 @@ for (const level in levels){
     result.push(<SkyscaperLevel  key={level + i} type={level}/>)
 }
  }  
+
+// return ( 
+//     <div className = {classes.SkyscaperPreview}>
+//         <div className={classes.levels}> 
+//              {/* <div className ={classes.levels}/>  */}
+//              <div className={classes.rocketImage}></div>
+//              <div className={classes.birdImage}></div>
+//              <div className={classes.cloudImage}></div>
+// {/*         <div className={classes.background}></div> */}
+//              <div className={classes.cloImage}></div>
+//              <div className={classes.seaImage}></div>
+//              <div className={classes.tree1Image}></div>
+//             {result}
+//         </div>
+//     <div className={classes.price}>{price.toFixed(1)}som</div>
+//     </div>
+// );
+
 return ( 
     <div className = {classes.SkyscaperPreview}>
         <div className={classes.levels}> 
-             <div className ={classes.levels}/> 
              <div className={classes.rocketImage}></div>
              <div className={classes.birdImage}></div>
              <div className={classes.cloudImage}></div>
-{/*         <div className={classes.background}></div> */}
              <div className={classes.cloImage}></div>
              <div className={classes.seaImage}></div>
              <div className={classes.tree1Image}></div>
+             <SkyscaperLevel type="floor0" />
             {result}
         </div>
     <div className={classes.price}>{price.toFixed(1)}som</div>
     </div>
-);}
+);
+
+
+}
 export default SkyscaperPreview;
 
-  
-  
+
