0b844c84413a094ba1be95fc0a78496a038d41e1
Your Name <you@example.com>
2021-05-14 02:43:57 -0700

    added some pictures in tis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-// import "./App.css";
-// import Layout from "./components/Layout/Layout";
-// import SkyscaperBuilder from "./components/SkyscaperBuilder/SkyscaperBuilder";
-// import Checkout from "./components/Checkout/Checkout"
-// import { Redirect, Route, Switch } from "react-router";
-// function App() {
-//   return (
-//     <div className="App">
-//      <Layout>
-//      <Switch>
-//           <Route path="/" component={SkyscaperBuilder} exact />
-//           <Route path="/checkout" component={Checkout} />
-//           <Redirect to="/" />
-//         </Switch>
-//        <h1>Skyscaper builder by Soltonbekova Aiym </h1>
-//      </Layout>
-//     </div>
-//   );
-// }
-// export default App;
 
 import Layout from "./components/Layout/Layout";
 import SkyscaperBuilder from "./components/SkyscaperBuilder/SkyscaperBuilder";
