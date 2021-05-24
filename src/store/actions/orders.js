a7d27053fc0540f1f92be7d8328e9401557862cc
Your Name <you@example.com>
2021-05-24 06:48:24 -0700

    added soe chnges

--- src/store/actions/orders.js
+++ src/store/actions/orders.js
@@ -1,3 +1,17 @@
+// import axios from "../../axios";
+// import { SET_ORDERS } from "./types";
+
+// export const set = (data) => ({
+//   type: SET_ORDERS,
+//   data: data
+// });
+
+// export const load = (token, id) => {
+//   return (dispatch) => axios
+//     .get('/orders.json?auth=' + token + '&orderBy="userId"&equalTo="' + id + '"')
+//     .then(response => dispatch(set(response.data)));
+// }
+
 import axios from "../../axios";
 import { SET_ORDERS } from "./types";
 
