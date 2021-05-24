8487dc6b304f7f0e3edd1b75535e2e321492a173
Your Name <you@example.com>
2021-05-24 06:50:49 -0700

    corected spome mistakes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 
+// import Layout from "./components/Layout/Layout";
+// import SkyscaperBuilder from "./components/SkyscaperBuilder/SkyscaperBuilder";
+// import Checkout from "./components/Checkout/Checkout";
+// import Orders from "./components/Orders/Orders";
+
+// import "./App.css";
+// import { Redirect, Route, Switch } from "react-router";
+// import Auth from "./components/Auth/Auth";
+// import Logout from "./components/Logout/Logout";
+// import { useDispatch } from "react-redux";
+// import { useSelector } from "react-redux";
+// import { useEffect } from "react";
+// import { restore } from "./store/actions/auth";
+
+// const App = () => {
+//   const dispatch = useDispatch();
+//   const isAuthenticated = useSelector(state => state.auth.token !== null);
+
+//   useEffect(() => restore(dispatch), [dispatch]);
+
+//   let routesOutput = (
+//     <Switch>
+//       <Route path="/" exact component={SkyscaperBuilder} />
+//       <Route path="/auth" component={Auth} />
+//       <Redirect to="/" />
+//     </Switch>
+//   );
+//   if (isAuthenticated) {
+//     routesOutput = (
+//       <Switch>
+//         <Route path="/" exact component={SkyscaperBuilder} />
+//         <Route path="/checkout" component={Checkout} />
+//         <Route path="/orders" component={Orders} />
+//         <Route path="/auth" component={Auth} />
+//         <Route path="/logout" component={Logout} />
+//         <Redirect to="/" />
+//       </Switch>
+//     );
+//   }
+
+//   return (
+//     <div className="App">
+//       <Layout>
+//         {routesOutput}
+//       </Layout>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import Layout from "./components/Layout/Layout";
 import SkyscaperBuilder from "./components/SkyscaperBuilder/SkyscaperBuilder";
 import Checkout from "./components/Checkout/Checkout";
 import Orders from "./components/Orders/Orders";
-
 import "./App.css";
 import { Redirect, Route, Switch } from "react-router";
 import Auth from "./components/Auth/Auth";
--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import {
+//   createStore,
+//   applyMiddleware,
+//   combineReducers
+// } from 'redux';
+// import thunk from 'redux-thunk';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import builder from './store/reducers/builder';
+// import orders from './store/reducers/orders';
+// import auth from './store/reducers/auth';
+
+// const rootReducer = combineReducers({ builder, orders, auth });
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -29,4 +63,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
