d3b7a41cf478859057a5660df5b6fa347b971cdb
Your Name <you@example.com>
2021-05-24 04:03:26 -0700

    фввув ыщьу срфтпуы

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,6 +1,8 @@
 
-  
-// import React, { useState } from "react";
+
+
+
+// import React from "react";
 // import axios from "../../axios";
 // import { start, auth } from "../../store/actions/auth";
 // import withAxios from "../withAxios";
@@ -11,7 +13,6 @@
 
 // export default withAxios(() => {
 //   const dispatch = useDispatch();
-//   const [ method, setMethod ] = useState(null);
 //   const { loading, error, token } = useSelector(state => state.auth);
 //   const location = useLocation();
 
@@ -19,6 +20,8 @@
 //     start(dispatch);
 
 //     const data = new FormData(event.target);
+//     const method = event.nativeEvent.submitter.innerText === "sign in"
+//       ? "signin" : "signup";
 //     auth(dispatch, method, data.get('email'), data.get('password'));
 
 //     event.preventDefault();
@@ -31,8 +34,8 @@
 //         <h1>Welcome</h1>
 //         <input type="email" placeholder="E-mail" name="email" required />
 //         <input type="password" placeholder="Password" name="password" required minLength="6" />
-//         <Button click={() => setMethod('signin')} green>Sign in</Button>
-//         <Button click={() => setMethod('signup')} red>Sign up</Button>
+//         <Button>Sign in</Button>
+//         <Button>Sign up</Button>
 //       </form>
 //     );
 //   }
@@ -56,66 +59,6 @@
 //     </div>
 //   );
 // }, axios);
-
-
-
-import React from "react";
-import axios from "../../axios";
-import { start, auth } from "../../store/actions/auth";
-import withAxios from "../withAxios";
-import classes from "./Auth.module.css";
-import Button from "../UI/Button/Button";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useLocation } from "react-router-dom";
-
-export default withAxios(() => {
-  const dispatch = useDispatch();
-  const { loading, error, token } = useSelector(state => state.auth);
-  const location = useLocation();
-
-  const formSubmitted = (event) => {
-    start(dispatch);
-
-    const data = new FormData(event.target);
-    const method = event.nativeEvent.submitter.innerText === "sign in"
-      ? "signin" : "signup";
-    auth(dispatch, method, data.get('email'), data.get('password'));
-
-    event.preventDefault();
-  }
-
-  let formOutput = "Loading...";
-  if (!loading) {
-    formOutput = (
-      <form onSubmit={formSubmitted}>
-        <h1>Welcome</h1>
-        <input type="email" placeholder="E-mail" name="email" required />
-        <input type="password" placeholder="Password" name="password" required minLength="6" />
-        <Button>Sign in</Button>
-        <Button>Sign up</Button>
-      </form>
-    );
-  }
-
-  let errorOutput = null;
-  if (error) {
-    errorOutput = <h4 className={classes.error}>{error.message}</h4>;
-  }
-
-  const [,redirect] = location.search.split('?');
-  let redirectOutput = null;
-  if (token !== null) {
-    redirectOutput = <Redirect to={"/" + redirect ? redirect : ""} />;
-  }
-
-  return (
-    <div className={classes.Auth}>
-      {errorOutput}
-      {formOutput}
-      {redirectOutput}
-    </div>
-  );
-}, axios);
 
 
 
@@ -158,3 +101,69 @@ export default withAxios(() => {
 //algoritmy struktury dannyh      fl.ru
 //pozl   portfilio                                                            
                        
+
+  
+import React from "react";
+import axios from "../../axios";
+import { start, auth } from "../../store/actions/auth";
+import withAxios from "../withAxios";
+import classes from "./Auth.module.css";
+import Button from "../Ul/Button/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, useLocation } from "react-router-dom";
+
+export default withAxios(() => {
+  const dispatch = useDispatch();
+  const { loading, error, token } = useSelector((state) => state.auth);
+  const location = useLocation();
+
+  const formSubmitted = (event) => {
+    start(dispatch);
+
+    const data = new FormData(event.target);
+    const method = event.nativeEvent.submitter.innerText === "sign in" ? "signin" : "signup";
+    auth(dispatch, method, data.get("email"), data.get("password"));
+
+    event.preventDefault();
+  };
+
+  let formOutput = "Loading...";
+  if (!loading) {
+    formOutput = (
+      <form onSubmit={formSubmitted}>
+        <h1>Welcome</h1>
+        <div className={classes.Auth}>
+          <input type="email" placeholder="E-mail" name="email" required />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+            minLength="6"
+          />
+        </div>
+        <Button> Sign up </Button>
+        <Button> Sign in </Button>
+      </form>
+    );
+  }
+
+  let errorOutput = null;
+  if (error) {
+    errorOutput = <h4 className={classes.error}>{error.message}</h4>;
+  }
+
+  const [, redirect] = location.search.split("?");
+  let redirectOutput = null;
+  if (token !== null) {
+    redirectOutput = <Redirect to={"/" + redirect ? redirect : ""} />;
+  }
+
+  return (
+    <div className={classes.Auth}>
+      {errorOutput}
+      {formOutput}
+      {redirectOutput}
+    </div>
+  );
+}, axios);
