07250f403549f5692ec430035e312e0c4d012681
Your Name <you@example.com>
2021-05-24 01:22:33 -0700

    added soe imafes

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,6 +1,65 @@
 
   
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import axios from "../../axios";
+// import { start, auth } from "../../store/actions/auth";
+// import withAxios from "../withAxios";
+// import classes from "./Auth.module.css";
+// import Button from "../UI/Button/Button";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Redirect, useLocation } from "react-router-dom";
+
+// export default withAxios(() => {
+//   const dispatch = useDispatch();
+//   const [ method, setMethod ] = useState(null);
+//   const { loading, error, token } = useSelector(state => state.auth);
+//   const location = useLocation();
+
+//   const formSubmitted = (event) => {
+//     start(dispatch);
+
+//     const data = new FormData(event.target);
+//     auth(dispatch, method, data.get('email'), data.get('password'));
+
+//     event.preventDefault();
+//   }
+
+//   let formOutput = "Loading...";
+//   if (!loading) {
+//     formOutput = (
+//       <form onSubmit={formSubmitted}>
+//         <h1>Welcome</h1>
+//         <input type="email" placeholder="E-mail" name="email" required />
+//         <input type="password" placeholder="Password" name="password" required minLength="6" />
+//         <Button click={() => setMethod('signin')} green>Sign in</Button>
+//         <Button click={() => setMethod('signup')} red>Sign up</Button>
+//       </form>
+//     );
+//   }
+
+//   let errorOutput = null;
+//   if (error) {
+//     errorOutput = <h4 className={classes.error}>{error.message}</h4>;
+//   }
+
+//   const [,redirect] = location.search.split('?');
+//   let redirectOutput = null;
+//   if (token !== null) {
+//     redirectOutput = <Redirect to={"/" + redirect ? redirect : ""} />;
+//   }
+
+//   return (
+//     <div className={classes.Auth}>
+//       {errorOutput}
+//       {formOutput}
+//       {redirectOutput}
+//     </div>
+//   );
+// }, axios);
+
+
+
+import React from "react";
 import axios from "../../axios";
 import { start, auth } from "../../store/actions/auth";
 import withAxios from "../withAxios";
@@ -11,7 +70,6 @@ import { Redirect, useLocation } from "react-router-dom";
 
 export default withAxios(() => {
   const dispatch = useDispatch();
-  const [ method, setMethod ] = useState(null);
   const { loading, error, token } = useSelector(state => state.auth);
   const location = useLocation();
 
@@ -19,6 +77,8 @@ export default withAxios(() => {
     start(dispatch);
 
     const data = new FormData(event.target);
+    const method = event.nativeEvent.submitter.innerText === "sign in"
+      ? "signin" : "signup";
     auth(dispatch, method, data.get('email'), data.get('password'));
 
     event.preventDefault();
@@ -31,8 +91,8 @@ export default withAxios(() => {
         <h1>Welcome</h1>
         <input type="email" placeholder="E-mail" name="email" required />
         <input type="password" placeholder="Password" name="password" required minLength="6" />
-        <Button click={() => setMethod('signin')} green>Sign in</Button>
-        <Button click={() => setMethod('signup')} red>Sign up</Button>
+        <Button>Sign in</Button>
+        <Button>Sign up</Button>
       </form>
     );
   }
@@ -56,9 +116,6 @@ export default withAxios(() => {
     </div>
   );
 }, axios);
-
-
-
 
 
 
--- src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
+++ src/components/SkyscaperBuilder/SkyscaperControls/SkyscaperControls.js
@@ -1,29 +1,58 @@
 
- import Button from "../../UI/Button/Button";
+//  import Button from "../../UI/Button/Button";
+// import SkyscaperControl from "./SkyscaperControl/SkyscaperControl";
+//  import classes from "./SkyscaperControls.module.css";
+// const SkyscaperControls = ({ 
+//   levels,
+//    startOrdering
+//   }) => {
+//   const results = [];
+//   let total = 0;
+//   for (const level in levels) {
+//   total+= levels[level];
+//     results.push( <SkyscaperControl
+//         type={level}
+//         key={level}
+//              count={levels[level]}   /> )
+//   }
+// return (
+//   <div className={classes.SkyscaperControls}>  
+//   <strong>Levels</strong>
+//   {results}
+//   <Button disabled={!total} onClick={startOrdering}>Order</Button>
+//   </div>
+// );
+// };
+// export default SkyscaperControls;
+
+
+
+
+
+
+import Button from "../../UI/Button/Button";
 import SkyscaperControl from "./SkyscaperControl/SkyscaperControl";
- import classes from "./SkyscaperControls.module.css";
-const SkyscaperControls = ({ 
-  levels,
-   startOrdering
-  }) => {
-  const results = [];
-  let total = 0;
-  for (const level in levels) {
-  total+= levels[level];
-    results.push( <SkyscaperControl
-        type={level}
-        key={level}
-             count={levels[level]}   /> )
-  }
-return (
-  <div className={classes.SkyscaperControls}>  
-  <strong>Levels</strong>
-  {results}
-  <Button disabled={!total} onClick={startOrdering}>Order</Button>
-  </div>
-);
-};
+import classes from "./SkyscaperControls.module.css";
+
+const SkyscaperControls = ({levels, startOrdering }) => {
+    const results = [];
+     let total = 0;
+    for (const level in levels) {
+        total += levels[level]
+         results.push(<SkyscaperControl type={level} key={level + levels} count={levels[level]}/>)
+    }
+        
+    return ( 
+        <div className={classes.SkyscaperControls}>
+           <div>
+           {results}
+           <div className={classes.Button}>
+           <Button onClick={() => startOrdering()} disabled={!total}><span>Order</span></Button>
+           </div>
+           
+           </div> 
+        </div>
+     );
+}
+ 
 export default SkyscaperControls;
-
-
-
